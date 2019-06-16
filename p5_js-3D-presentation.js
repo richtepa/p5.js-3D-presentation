bf7a3e914f90170704e4b949edf0dfe84e61032b
Richtepa <mail@richterpaul.de>
2019-06-16 18:39:20 +0200

    more user-friendly

--- p5_js-3D-presentation.js
+++ p5_js-3D-presentation.js
@@ -1,13 +1,3 @@
-/*
-
-
-RÜCKWÄRTS BLÄTTERN FUNTIONIERT NICHT!
-
-
-*/
-
-
-
 fps = 60;
 positionVariables = ["x", "y", "z", "rx", "ry", "rz"];
 models = new Object();
@@ -15,49 +5,57 @@ textures = new Object();
 actualSlide = 0;
 
 cam = new Cam();
-
-
-
 objects = new Array();
 for (model of slideData.objects) {
     data = Object.assign(model, slideData.slides[0][model.name]);
     objects.push(new Model(data));
 }
 
-showSlide(0, false);
+window.onload = function () {
+    createAllElements();
+    changeSlide(0, false);
+}
+
+
 
 function Cam() {
+    for (p of positionVariables) {
+        this[p] = 0;
+    }
     this.reset = function () {
         this.animate = false;
         this.visible = true;
         this.aim = new Object();
+        this.old = new Object();
         for (p of positionVariables) {
             this.aim[p] = 0;
+            this.old[p] = this[p];
         }
         this.duration = 0;
         this.progress = 0;
     }
 
     this.reset();
-    for (p of positionVariables) {
-        this[p] = 0;
-    }
 }
 
 
 function Model(data) {
+    for (p of positionVariables) {
+        this[p] = 0;
+    }
+
     this.reset = function () {
         this.animate = false;
         this.visible = true;
         this.aim = new Object();
+        this.old = new Object();
         for (p of positionVariables) {
             this.aim[p] = 0;
+            this.old[p] = this[p];
         }
         this.duration = 0;
         this.progress = 0;
     }
-
-
 
     this.name = data.name
     this.model = data.model;
@@ -65,91 +63,137 @@ function Model(data) {
     this.material = data.material;
     this.scale = data.scale;
     this.reset();
-    for (p of positionVariables) {
-        this[p] = 0;
-    }
 }
 
 
 
-function preload() {
-    for (obj of objects) {
-        models[obj.name] = loadModel(obj.model);
-        if (obj.texture != undefined) {
-            textures[obj.name] = loadImage(obj.texture);
+function createAllElements() {
+    structure = document.getElementById("structure");
+    ul = document.createElement("ul");
+    structure.appendChild(ul);
+    for (i = 0; i < slideData.slides.length; i++) {
+        slide = slideData.slides[i];
+        if (slide.title != undefined) {
+            e = document.createElement("li");
+            e.id = "structure" + i;
+            e.classList.add("structure");
+            e.innerHTML = slide.title;
+            e.addEventListener("click", function () {
+                actualSlide = this.id.split("structure")[1];
+                changeSlide(actualSlide, false);
+            });
+            ul.appendChild(e);
         }
     }
+
+    white = document.createElement("div");
+    white.id = "white";
+    white.classList.add("hidden");
+    document.body.appendChild(white);
+
+    black = document.createElement("div");
+    black.id = "black";
+    black.classList.add("hidden");
+    document.body.appendChild(black);
+
+    let presentation = new p5(pres, "pres3D");
 }
 
-function setup() {
-    createCanvas(windowWidth / 100 * 33, windowHeight, WEBGL);
-    angleMode(DEGREES);
-}
+const pres = function (sketch) {
 
-function draw() {
-    noStroke();
-    //background(240, 128, 128);
-    background(slideData.background[0], slideData.background[1], slideData.background[2]);
-
-    
-    //CAMERA
-    if (cam.animate) {
-        if (cam.progress < 1) {
-            cam.progress += 1 / (cam.duration * fps);
-            if (cam.progress > 1) {
-                cam.progress = 1;
+    sketch.preload = function () {
+        for (obj of objects) {
+            models[obj.name] = sketch.loadModel(obj.model);
+            if (obj.texture != undefined) {
+                textures[obj.name] = sketch.loadImage(obj.texture);
             }
         }
-        for (p of positionVariables) {
-            if (cam[p] != cam.aim[p]) {
-                cam[p] = (cam.progress * cam.aim[p]) + ((1 - cam.progress) * cam[p]);
-            }
-        }
     }
 
+    sketch.setup = function () {
+        sketch.createCanvas(document.getElementById("pres3D").clientWidth, this.height = document.getElementById("pres3D").clientHeight, sketch.WEBGL);
+        sketch.angleMode(sketch.DEGREES);
+    }
 
-    camera(
-        cam.x, cam.y, cam.z,
-        cam.rx, cam.ry, cam.rz,
-        0, 1, 0
-    );
+    sketch.windowResized = function () {
+        sketch.resizeCanvas(document.getElementById("pres3D").clientWidth, document.getElementById("pres3D").clientHeight);
+    }
 
+    sketch.draw = function () {
+        try {
+            // user implemented function
+            beforeDraw();
+        } catch {}
 
-    //OBJECTS
-    for (obj of objects) {
-        if (obj.animate) {
-            if (obj.progress < 1) {
-                obj.progress += 1 / (obj.duration * fps);
-                if (obj.progress > 1) {
-                    obj.progress = 1;
+        sketch.noStroke();
+        sketch.background(slideData.background[0], slideData.background[1], slideData.background[2]);
+
+        //CAMERA
+        if (cam.animate) {
+            if (cam.progress < 1) {
+                cam.progress += 1 / (cam.duration * fps);
+                if (cam.progress > 1) {
+                    cam.progress = 1;
+                }
+            } else {
+                if (slideData.slides[actualSlide].noClick) {
+                    next();
                 }
             }
             for (p of positionVariables) {
-                if (obj[p] != obj.aim[p]) {
-                    obj[p] = (obj.progress * obj.aim[p]) + ((1 - obj.progress) * obj[p]);
+                if (cam[p] != cam.aim[p]) {
+                    cam[p] = (cam.progress * cam.aim[p]) + ((1 - cam.progress) * cam.old[p]);
                 }
             }
         }
+        sketch.camera(
+            cam.x, cam.y, cam.z,
+            cam.rx, cam.ry, cam.rz,
+            0, 1, 0
+        );
 
-        
-        if (obj.visible) {
-            push();
-            translate(obj.x, obj.y, obj.z);
-            rotateX(obj.rx);
-            rotateY(obj.ry);
-            rotateZ(obj.rz);
-            scale(obj.scale);
-            if (textures[obj.name] != undefined) {
-                texture(textures[obj.name]);
-            } else {
-                fill(obj.material[0], obj.material[1], obj.material[2]);
+        //OBJECTS
+        for (obj of objects) {
+            if (obj.animate) {
+                if (obj.progress < 1) {
+                    obj.progress += 1 / (obj.duration * fps);
+                    if (obj.progress > 1) {
+                        obj.progress = 1;
+                    }
+                }
+                for (p of positionVariables) {
+                    if (obj[p] != obj.aim[p]) {
+                        obj[p] = (obj.progress * obj.aim[p]) + ((1 - obj.progress) * obj.old[p]);
+                    }
+                }
             }
-            model(models[obj.name]);
-            pop();
+
+
+            if (obj.visible) {
+                sketch.push();
+                sketch.translate(obj.x, obj.y, obj.z);
+                sketch.rotateX(obj.rx);
+                sketch.rotateY(obj.ry);
+                sketch.rotateZ(obj.rz);
+                sketch.scale(obj.scale);
+                if (textures[obj.name] != undefined) {
+                    sketch.texture(textures[obj.name]);
+                } else {
+                    sketch.fill(obj.material[0], obj.material[1], obj.material[2]);
+                }
+                sketch.model(models[obj.name]);
+                sketch.pop();
+            }
+
         }
 
+        try {
+            // user implemented function
+            afterDraw();
+        } catch {}
     }
 }
+
 
 document.addEventListener("keydown", function (e) {
     //console.log(e.keyCode);
@@ -170,16 +214,19 @@ document.addEventListener("keydown", function (e) {
     }
 });
 
-function windowResized() {
-    resizeCanvas(windowWidth * 33 / 100, windowHeight);
-}
-
 function before() {
     actualSlide--;
     if (actualSlide < 0) {
         actualSlide = 0;
     }
-    showSlide(actualSlide, false);
+    while (slideData.slides[actualSlide].noClick) {
+        actualSlide--;
+        if (actualSlide < 0) {
+            actualSlide = 0;
+            break;
+        }
+    }
+    changeSlide(actualSlide, false);
 }
 
 function next() {
@@ -187,21 +234,43 @@ function next() {
     if (actualSlide > slideData.slides.length - 1) {
         actualSlide = slideData.slides.length - 1;
     }
-    showSlide(actualSlide, true);
+    changeSlide(actualSlide, true);
+}
+
+function changeSlide(n, animate) {
+    try {
+        // user implemented function
+        slideActivated(n);
+    } catch {}
+    updateStructure(n);
+    updateSlide(n);
+    update3D(n, animate);
 }
 
 
-function showSlide(n, animate) {
-    slideActivated(n);
 
-    //TEXT
+function updateStructure(n) {
+    structures = document.getElementsByClassName("structure");
+    for (structure of structures) {
+        structure.classList.remove("active");
+    }
+    try {
+        document.getElementById("structure" + n).classList.add("active");
+    } catch {}
+}
+
+function updateSlide(n) {
     slides = document.getElementsByClassName("slide");
     for (slide of slides) {
-        slide.classList.add("hidden");
+        if (slide.id.split("slide")[1] <= n) {
+            slide.classList.remove("hidden");
+        } else {
+            slide.classList.add("hidden");
+        }
     }
-    document.getElementById("slide" + n).classList.remove("hidden");
+}
 
-
+function update3D(n, animate) {
     //CAMERA
     cam.reset();
     for (s = 0; s <= n; s++) {
--- slideData.js
+++ slideData.js
@@ -1,151 +1,184 @@
-//Camera rotation --> center
+// camera rotation --> centerpoint
+// all values (except noClick) add up trough the process
 
-        slideData = {
-            "background": [240, 128, 128],
-            "objects": [{
-                    "name": "table",
-                    "model": "3Dmodel/table/Table.obj",
-                    "material": [100, 50, 0],
-                    "scale": 0.5
+slideData = {
+    
+    // define your background and objects
+    "background": [240, 128, 128],
+    "objects": [{
+            "name": "table",
+            "model": "3Dmodel/table/Table.obj",
+        
+            // you can use rgb-colors as material
+            "material": [100, 50, 0],
+        
+            "scale": 0.5
                 },
-                {
-                    "name": "computer",
-                    "model": "3Dmodel/pc/PC.obj",
-                    "texture": "3Dmodel/pc/PC.png",
-                    "scale": 1
+        {
+            "name": "computer",
+            "model": "3Dmodel/pc/PC.obj",
+            "texture": "3Dmodel/pc/PC.png",
+            "scale": 1
                 },
-                {
-                    "name": "mouse",
-                    "model": "3Dmodel/mouse/ComputerMouse.obj",
-                    "texture": "3Dmodel/mouse/ComputerMouse.png",
-                    "scale": 4
+        {
+            "name": "mouse",
+            "model": "3Dmodel/mouse/ComputerMouse.obj",
+            
+            // or images as textures
+            "texture": "3Dmodel/mouse/ComputerMouse.png",
+            
+            "scale": 4
                 },
-                {
-                    "name": "monitor",
-                    "model": "3Dmodel/monitor/Monitor.obj",
-                    "texture": "3Dmodel/monitor/Monitor.png",
-                    "scale": 1
+        {
+            "name": "monitor",
+            "model": "3Dmodel/monitor/Monitor.obj",
+            "texture": "3Dmodel/monitor/Monitor.png",
+            "scale": 1
                 },
-                {
-                    "name": "keyboard",
-                    "model": "3Dmodel/keyboard/Keyboard.obj",
-                    "texture": "3Dmodel/keyboard/Keyboard.png",
-                    "scale": 1
+        {
+            "name": "keyboard",
+            "model": "3Dmodel/keyboard/Keyboard.obj",
+            "texture": "3Dmodel/keyboard/Keyboard.png",
+            "scale": 1
                 }
             ],
-            "slides": [{
-                    "duration": 0,
-                    "camera": {
-                        "ry": -40,
-                        "y": -100,
-                        "z": 150
-                    },
-                    "table": {
-                        "visible": false,
-                        "z": 40,
-                        "y": 90,
-                        "rx": 90,
+    "slides": [{
+            "duration": 0,
+            "camera": {
+                "ry": -40,
+                "y": -100,
+                "z": 150
+            },
+            "table": {
+                "visible": false,
+                "z": 40,
+                "y": 90,
+                "rx": 90,
 
-                    },
-                    "computer": {
-                        "visible": false,
-                        "x": 75,
-                        "y": 90,
-                        "z": 40,
-                        "ry": -90,
-                        "rz": 180
-                    },
-                    "mouse": {
-                        "visible": false,
-                        "x": 60,
-                        "y": 1,
-                        "z": 60,
-                        "ry": 180,
-                        "rz": 180
-                    },
-                    "monitor": {
-                        "rz": 180,
-                        "ry": -90
-                    },
-                    "keyboard": {
-                        "visible": false,
-                        "z": 50,
-                        "rz": 180
-                    }
+            },
+            "computer": {
+                "visible": false,
+                "x": 75,
+                "y": 90,
+                "z": 40,
+                "ry": -90,
+                "rz": 180
+            },
+            "mouse": {
+                "visible": false,
+                "x": 60,
+                "y": 1,
+                "z": 60,
+                "ry": 180,
+                "rz": 180
+            },
+            "monitor": {
+                "rz": 180,
+                "ry": -90
+            },
+            "keyboard": {
+                "visible": false,
+                "z": 50,
+                "rz": 180
+            }
                 },
-                {
-                    "duration": 2,
-                    "camera": {
-                        "ry": 40,
-                        "y": 100,
-                        "z": 350
-                    },
-                    "table": {
-                        "visible": true
-                    },
-                    "computer": {
-                        "visible": true
-                    },
-                    "mouse": {
-                        "visible": true
-                    },
-                    "monitor": {},
-                    "keyboard": {
-                        "visible": true
-                    }
+        {
+            
+            // titles are shown in the structure
+            "title": "Fits on every desk",
+            
+            "duration": 0.5,
+            "camera": {
+                "ry": 40,
+                "y": 100,
+                "z": 350
+            },
+            "table": {
+                "visible": true
+            },
+            "computer": {
+                "visible": true
+            },
+            "mouse": {
+                "visible": true
+            },
+            "monitor": {},
+            "keyboard": {
+                "visible": true
+            }
                 },
-                {
-                    "duration": 2,
-                    "camera": {
-                        "y": -100,
-                    },
-                    "table": {
-                        "x": -500
-                    },
-                    "computer": {
-                        "x": -500
-                    },
-                    "mouse": {},
-                    "monitor": {},
-                    "keyboard": {}
+        {
+            "title": "Specs",
+            "duration": 0.5,
+            "camera": {
+                "y": -100,
+            },
+            "table": {
+                "x": 500
+            },
+            "computer": {
+                "x": 500
+            },
+            "mouse": {},
+            "monitor": {},
+            "keyboard": {}
                 },
 
-                {
-                    "duration": 2,
-                    "camera": {
-                        "x": 100,
-                        "z": -300
-                    },
-                    "table": {
-                        "visible": false
-                    },
-                    "computer": {
-                        "visible": false
-                    },
-                    "mouse": {
-                        "z": 200
-                    },
-                    "monitor": {},
-                    "keyboard": {
-                        "z": 200
-                    }
+        {
+            "title": "Screen specs",
+            "duration": 0.5,
+            "camera": {
+                "x": -100,
+                "z": -300
+            },
+            "table": {
+                "visible": false
+            },
+            "computer": {
+                "visible": false
+            },
+            "mouse": {
+                "z": 200
+            },
+            "monitor": {},
+            "keyboard": {
+                "z": 200
+            },
                 },
-                {
-                    "duration": 2,
-                    "camera": {
-                        "ry": -40,
-                        "x": -100,
-                        "z": -50
-                    },
-                    "table": {},
-                    "computer": {},
-                    "mouse": {
-                        "visible": false
-                    },
-                    "monitor": {},
-                    "keyboard": {
-                        "visible": false
-                    }
+        {
+            
+            // with noClick the next slide gets shown after duration
+            "noClick": true,
+            
+            "duration": 0.5,
+            "camera": {
+                "ry": -40,
+                "x": 100,
+                "z": -50
+            },
+            "table": {},
+            "computer": {},
+            "mouse": {
+                "visible": false
+            },
+            "monitor": {},
+            "keyboard": {
+                "visible": false
+            }
+                },
+        {
+            "title": "Price",
+            "duration": 0.5,
+            "camera": {
+                "ry": 40,
+                "x": 100,
+                "z": 50
+            },
+            "table": {},
+            "computer": {},
+            "mouse": {},
+            "monitor": {},
+            "keyboard": {}
                 }
             ]
-        };
+};
